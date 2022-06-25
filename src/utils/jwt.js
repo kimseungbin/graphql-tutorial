74dbecd72d99ab583a4a35536a09d925f690060c
SeungBin Kim <seungbin0508@gmail.com>
2022-06-25 14:35:12 +0900

    Implement createToken util function

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,3 +1,8 @@
-export function createToken(payload) {
+import jwt from 'jsonwebtoken';
 
+export function createToken(payload) {
+    // noinspection JSCheckFunctionSignatures
+    return jwt.sign(payload, 'jwt-secret', {
+        expiresIn: '2 days'
+    });
 }
