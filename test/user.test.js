d231937274d9c98510562ab4e317a215e561051f
SeungBin Kim <seungbin0508@gmail.com>
2022-06-25 13:48:30 +0900

    Refactor test code to test for invalid input

--- test/user.test.js
+++ test/user.test.js
@@ -7,33 +7,60 @@ import {Mutation} from "../src/resolvers/index.js";
 
 const testServer = new ApolloServer({
     typeDefs: await fs.readFile(path.join(path.resolve(), 'docs/graphql/schema.graphql'), 'utf-8'),
-    resolvers: { Mutation }
+    resolvers: {Mutation}
 });
 
 test('sign up', async t => {
-    const graphQLResponse = await testServer.executeOperation({
-        // language=GraphQL
-        query: `
-            mutation Signup($input: SignupInput!) {
-                signup(input: $input) {
-                    token
-                    user {
-                        name
-                        email
+    await t.test('should return name and email', async t => {
+        const graphQLResponse = await testServer.executeOperation({
+            // language=GraphQL
+            query: `
+                mutation Signup($input: SignupInput!) {
+                    signup(input: $input) {
+                        token
+                        user {
+                            name
+                            email
+                        }
                     }
                 }
+            `,
+            variables: {
+                input: {
+                    email: 'seungbin0508@gmail.com',
+                    password: 'hello',
+                    name: 'SeungBin Kim',
+                }
             }
-        `,
-        variables: {
-            input: {
-                email: 'seungbin0508@gmail.com',
-                password: 'hello',
-                name: 'SeungBin Kim',
-            }
-        }
+        });
+        const {errors, data: {signup: {user: {name, email}}}} = graphQLResponse;
+        assert.equal(errors, undefined);
+        assert.equal(name, 'SeungBin Kim');
+        assert.equal(email, 'seungbin0508@gmail.com');
     });
-    const {errors, data: {signup: {user: {name, email}}}} = graphQLResponse;
-    assert.equal(errors, undefined)
-    assert.equal(name, 'SeungBin Kim')
-    assert.equal(email, 'seungbin0508@gmail.com')
+
+    await t.test('should fail signing up due to invalid input', async t => {
+        const graphQLResponse = await testServer.executeOperation({
+            // language=GraphQL
+            query: `
+                mutation InvalidSignup($input: SignupInput!) {
+                    signup(input: $input) {
+                        token
+                        user {
+                            name
+                            email
+                        }
+                    }
+                }
+            `,
+            variables: {
+                input: {
+
+                }
+            }
+        });
+
+        const {errors} = graphQLResponse;
+        assert.equal(!!errors, true)
+    })
 });
