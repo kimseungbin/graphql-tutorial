1db3b2b99dd6610f40cd2fd952e7e0d425c48422
SeungBin Kim <seungbin0508@gmail.com>
2022-06-25 15:11:16 +0900

    Add test for login

---
+++ test/integration/login.test.js
@@ -0,0 +1,52 @@
+import test from "node:test";
+import {ApolloServer} from "apollo-server";
+import fs from "fs/promises";
+import path from "path";
+import assert from "node:assert/strict";
+import jwt from "jsonwebtoken";
+import {Mutation} from "../../src/resolvers/index.js";
+
+test('login', async t => {
+    const testServer = new ApolloServer({
+        typeDefs: await fs.readFile(path.join(path.resolve(), 'docs/graphql/schema.graphql'), 'utf-8'),
+        resolvers: {Mutation}
+    });
+    async function queryLogin(input) {
+        return await testServer.executeOperation({
+            // language=GraphQL
+            query: `
+                mutation Login($input: LoginInput!) {
+                    login(input: $input) {
+                        token
+                        user {
+                            name
+                            email
+                        }
+                    }
+                }
+            `,
+            variables: input
+        });
+    }
+
+    t.test('should return token', async () => {
+        // todo insert a user into db
+
+        const res = await queryLogin({
+            email: 'seungbin0508@gmail.com',
+            password: 'apple'
+        });
+        console.log('hi');
+
+        const {errors, data: {login: {token, user: {name, email}}}} = res
+        assert.equal(errors, undefined);
+        try {
+            // noinspection JSCheckFunctionSignatures
+            jwt.verify(token, 'jwt-secret');
+        } catch (e) {
+            assert.ifError(e)
+        }
+        assert.equal(name, 'SeungBin Kim');
+        assert.equal(email, 'seungbin0508@gmail.com');
+    });
+});
