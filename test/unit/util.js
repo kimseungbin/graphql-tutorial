6ea400484306ee74f8bf72a757326eb9e3380e31
SeungBin Kim <seungbin0508@gmail.com>
2022-06-25 14:15:06 +0900

    Add test for createToken

---
+++ test/unit/util.js
@@ -0,0 +1,19 @@
+import test from "node:test";
+import {createToken} from "../../src/utils/jwt.js";
+import jwt from 'jsonwebtoken';
+import assert from "node:assert/strict";
+
+test('createToken', t => {
+    const payload = {
+        email: 'seungbin0508@gmail.com',
+        name: 'SeungBin Kim'
+    }
+
+    const token = createToken(payload)
+    const returnPayload = jwt.decode(token, {})
+
+    const twoDaysInSec = 60 * 60 * 24 * 2
+    assert.equal(payload.email, returnPayload.email)
+    assert.equal(payload.name, returnPayload.name)
+    assert.equal(returnPayload.exp - returnPayload.iat, twoDaysInSec)
+})
