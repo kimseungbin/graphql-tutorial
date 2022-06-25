707a7cd058396dae5ce3eae287898b0f786721d1
SeungBin Kim <seungbin0508@gmail.com>
2022-06-25 14:41:37 +0900

    Add test for jwt in signup

--- test/user.test.js
+++ test/user.test.js
@@ -4,6 +4,7 @@ import * as fs from "fs/promises";
 import * as path from "path";
 import assert from "node:assert/strict";
 import {Mutation} from "../src/resolvers/index.js";
+import jwt from 'jsonwebtoken';
 
 const testServer = new ApolloServer({
     typeDefs: await fs.readFile(path.join(path.resolve(), 'docs/graphql/schema.graphql'), 'utf-8'),
@@ -11,7 +12,7 @@ const testServer = new ApolloServer({
 });
 
 test('sign up', async t => {
-    await t.test('should return name and email', async t => {
+    await t.test('should return token and user data', async t => {
         const graphQLResponse = await testServer.executeOperation({
             // language=GraphQL
             query: `
@@ -33,8 +34,14 @@ test('sign up', async t => {
                 }
             }
         });
-        const {errors, data: {signup: {user: {name, email}}}} = graphQLResponse;
+        const {errors, data: {signup: {token, user: {name, email}}}} = graphQLResponse;
         assert.equal(errors, undefined);
+        try {
+            // noinspection JSCheckFunctionSignatures
+            jwt.verify(token, 'jwt-secret');
+        } catch (e) {
+            assert.ifError(e);
+        }
         assert.equal(name, 'SeungBin Kim');
         assert.equal(email, 'seungbin0508@gmail.com');
     });
@@ -54,13 +61,11 @@ test('sign up', async t => {
                 }
             `,
             variables: {
-                input: {
-
-                }
+                input: {}
             }
         });
 
         const {errors} = graphQLResponse;
-        assert.equal(!!errors, true)
-    })
+        assert.equal(!!errors, true);
+    });
 });
