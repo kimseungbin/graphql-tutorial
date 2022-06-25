9e95dbdb0fc300e42f1cb1118ef1fd2b87b4d2b3
SeungBin Kim <seungbin0508@gmail.com>
2022-06-25 15:24:41 +0900

    Implement login mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,14 +1,31 @@
 import {createToken} from "../utils/jwt.js";
 
-export async function signup (parent, args, context, info) {
+export async function signup(parent, args, context, info) {
     const {input: {name, email}} = args;
-    const token = createToken({name, email})
+    const token = createToken({name, email});
     return {
         token,
         user: {
-            _id: '1',
             name,
             email
         }
-    }
+    };
+}
+
+export async function login(parent, args, context, info) {
+    // todo get user name from db
+    // todo check if user exists
+    // todo validate password
+    const name = 'SeungBin Kim';
+
+    const {input: {email, password}} = args;
+    const token = createToken({name, email});
+    return {
+        token,
+        user: {
+            name,
+            email
+        }
+    };
+
 }
