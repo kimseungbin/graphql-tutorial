c5c7864b53d94752dee14bd471405034ebb38a70
SeungBin Kim <seungbin0508@gmail.com>
2022-06-25 14:45:07 +0900

    Implement token in signup

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,7 +1,10 @@
+import {createToken} from "../utils/jwt.js";
+
 export async function signup (parent, args, context, info) {
     const {input: {name, email}} = args;
+    const token = createToken({name, email})
     return {
-        token: '12345',
+        token,
         user: {
             _id: '1',
             name,
