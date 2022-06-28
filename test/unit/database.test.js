5d855d3c43c4296b3140967f34f5b1886424644c
SeungBin Kim <seungbin0508@gmail.com>
2022-06-28 20:08:30 +0900

    Add member find test

--- test/unit/database.test.js
+++ test/unit/database.test.js
@@ -4,6 +4,11 @@ import createInMemoryConnection from "../../src/utils/inMemoryDB.js";
 import {ObjectId} from 'mongodb';
 import {createMember} from "../../src/user/db.js";
 
+const member = {
+    name: 'SeungBin Kim',
+    email: 'seungbin0508@gmail.com',
+    password: 'abc123'
+};
 
 await test('Connect to database', async t => {
     let connection;
@@ -18,14 +23,25 @@ await test('Connect to database', async t => {
 
 await test('Create a member account', async t => {
     const client = await createInMemoryConnection();
-    const member = {
-        name: 'SeungBin Kim',
-        email: 'seungbin0508@gmail.com',
-        password: 'abc123'
-    };
+
     try {
         const insertedId = await createMember({client}, member);
         const result = ObjectId.isValid(insertedId);
+        assert.ok(result);
+    } catch (e) {
+        assert.ifError(e);
+    } finally {
+        await client.stopDatabase();
+    }
+});
+
+await test('Find a member account', async t => {
+    const client = await createInMemoryConnection();
+
+    try {
+        const insertedId = await createMember({client}, member);
+        const member = await findMember({client}, insertedId);
+        const result = Object.entries(member).every(([key, value]) => member[key] === value);
         assert.ok(result);
     } catch (e) {
         assert.ifError(e);
