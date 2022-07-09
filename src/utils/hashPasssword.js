956908ea80b74f3b7d07c805e2305939792456ac
SeungBin Kim <seungbin0508@gmail.com>
2022-07-09 11:08:59 +0900

    Refactor password hashing to take second optional parameter salt

--- src/utils/hashPasssword.js
+++ src/utils/hashPasssword.js
@@ -3,13 +3,13 @@ import crypto from "crypto";
 /**
  *
  * @param password {string}
+ * @param salt {string=}
  * @returns {{salt: string, hash: string}}
  */
-export default function hashPassword(password) {
-    const salt = crypto.randomBytes(16).toString('hex');
+export default function hashPassword(password, salt) {
+    salt ??= crypto.randomBytes(16).toString("hex");
 
-    const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
+    const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, "sha512").toString("hex");
 
     return {salt, hash};
-
 }
