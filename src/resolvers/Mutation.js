6e2721f288bf261923dbfa24e3e360c0a30a3a4d
SeungBin Kim <seungbin0508@gmail.com>
2022-07-13 07:48:45 +0900

    Refactor login.test.js to use try/catch block

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 import {createToken} from "../utils/jwt.js";
 import {createMember, findMember} from "../user/db.js";
+import hashPassword from "../utils/hashPasssword.js";
+import {UserInputError} from "apollo-server";
 
 export async function signup(parent, args, ctx, info) {
     const {input, input: {name, email}} = args;
@@ -22,15 +24,28 @@ export async function login(parent, args, context, info) {
     // todo get user name from db
     // todo check if user exists
     // todo validate password
-    const name = "SeungBin Kim";
-
-    const {input: {email, password}} = args;
-    const token = createToken({name, email});
-    return {
-        token,
-        user: {
-            name,
-            email
+    try {
+        const {input: {email, password}} = args;
+        const filter = {email};
+        const member = await findMember(context, filter);
+        const {password: savedPassword, salt} = member;
+        const {hash: hashedPassword} = hashPassword(password, salt);
+        if (hashedPassword !== savedPassword) {
+            return new UserInputError("잘못된 비밀번호입니다.");
         }
-    };
+
+        const {name} = member;
+        console.log(savedPassword, hashedPassword);
+        const token = createToken({name, email});
+        return {
+            token,
+            user: {
+                name,
+                email
+            }
+        };
+    } catch (e) {
+
+    }
+
 }
